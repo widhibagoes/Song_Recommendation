ff37fe900341d7fd4fb8ed1c33aabb50c12ad959
widhibagoes <widhi.bagoes.wb@gmail.com>
2023-12-19 16:10:54 +0700

    update

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ df = pd.read_csv('music_list.csv')
 doc_sim_df = pd.read_csv('similarity.csv')
 
 musics_list = df['track_name'].values
+artist_name = df['artist_name'].values
 
-def musics_recommender(musics_title, musics=musics_list, doc_sims=doc_sim_df):
+def musics_recommender(musics_title, musics=musics_list, artist=artist_name, doc_sims=doc_sim_df):
     # find movie id
     musics_idx = np.where(musics == musics_title)[0][0]
     # get movie similarities
@@ -16,8 +17,14 @@ def musics_recommender(musics_title, musics=musics_list, doc_sims=doc_sim_df):
     similar_musics_idxs = np.argsort(-musics_similarities)[1:6]
     # get top 5 movies
     similar_musics = musics[similar_musics_idxs]
+    similar_artist = artist[similar_musics_idxs]
+
+    similar_data = pd.DataFrame({
+        'title': similar_musics,
+        'artist': similar_artist
+    })
     # return the top 5 movies
-    return similar_musics
+    return similar_data
 
 st.title('Sistem Rekomendasi Musik 🎵')
 st.header('Menggunakan algoritma TF-IDF dan Cosine Similarity', divider='violet')
